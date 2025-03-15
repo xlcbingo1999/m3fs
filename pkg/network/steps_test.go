755956bd6d894cc4b1091a56a686846218ba829b
Jiajun Liu <iamljj@gmail.com>
2025-03-15 16:38:43 +0800

    add cluster network prepare command
    Signed-off-by: Jiajun Liu <iamljj@gmail.com>

---
+++ pkg/network/steps_test.go
@@ -0,0 +1,136 @@
+package network
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+
+	"github.com/open3fs/m3fs/pkg/config"
+	ttask "github.com/open3fs/m3fs/tests/task"
+)
+
+var suiteRun = suite.Run
+
+func TestGenIbdev2netdevScriptStep(t *testing.T) {
+	suiteRun(t, &genIbdev2netdevScriptStepSuite{})
+}
+
+type genIbdev2netdevScriptStepSuite struct {
+	ttask.StepSuite
+
+	step *genIbdev2netdevScriptStep
+}
+
+func (s *genIbdev2netdevScriptStepSuite) SetupTest() {
+	s.StepSuite.SetupTest()
+
+	s.step = &genIbdev2netdevScriptStep{}
+	s.Cfg.Nodes = []config.Node{
+		{
+			Name: "node1",
+			Host: "1.1.1.1",
+		},
+	}
+	s.SetupRuntime()
+	s.step.Init(s.Runtime, s.MockEm, s.Cfg.Nodes[0])
+}
+
+func (s *genIbdev2netdevScriptStepSuite) TestGenIbdev2netdevScript() {
+	tmpDir := "/tmp/m3fs-prepare-network.123"
+	s.MockLocalFS.On("MkdirTemp", "/tmp", "m3fs-prepare-network.*").Return(tmpDir, nil)
+	scriptPath := tmpDir + "/ibdev2netdev"
+	s.MockLocalFS.On("WriteFile", scriptPath,
+		[]byte(ibdev2netdevScript), os.FileMode(0755)).Return(nil)
+
+	binDir := s.Cfg.WorkDir + "/bin"
+	remoteScriptPath := binDir + "/ibdev2netdev"
+	s.MockRunner.On("Exec", "mkdir", []string{"-p", binDir}).Return("", nil)
+	s.MockRunner.On("Scp", scriptPath, remoteScriptPath).Return(nil)
+	s.MockRunner.On("Exec", "chmod", []string{"+x", remoteScriptPath}).Return("", nil)
+
+	s.MockLocalFS.On("RemoveAll", tmpDir).Return(nil)
+
+	s.NoError(s.step.Execute(s.Ctx()))
+
+	s.MockLocalFS.AssertExpectations(s.T())
+	s.MockRunner.AssertExpectations(s.T())
+}
+
+func TestLoadRdmaRxeModule(t *testing.T) {
+	suiteRun(t, &loadRdmaRxeModuleStepSuite{})
+}
+
+type loadRdmaRxeModuleStepSuite struct {
+	ttask.StepSuite
+
+	step *loadRdmaRxeModuleStep
+}
+
+func (s *loadRdmaRxeModuleStepSuite) SetupTest() {
+	s.StepSuite.SetupTest()
+
+	s.step = &loadRdmaRxeModuleStep{}
+	s.Cfg.Nodes = []config.Node{
+		{
+			Name: "node1",
+			Host: "1.1.1.1",
+		},
+	}
+	s.SetupRuntime()
+	s.step.Init(s.Runtime, s.MockEm, s.Cfg.Nodes[0])
+}
+
+func (s *loadRdmaRxeModuleStepSuite) TestLoadRdmaRxeModule() {
+	s.MockRunner.On("Exec", "modprobe", []string{"rdma_rxe"}).Return("", nil)
+
+	s.NoError(s.step.Execute(s.Ctx()))
+
+	s.MockRunner.AssertExpectations(s.T())
+}
+
+func TestCreateRdmaRxeLinkStep(t *testing.T) {
+	suiteRun(t, &createRdmaRxeLinkStepSuite{})
+}
+
+type createRdmaRxeLinkStepSuite struct {
+	ttask.StepSuite
+
+	step *createRdmaRxeLinkStep
+}
+
+func (s *createRdmaRxeLinkStepSuite) SetupTest() {
+	s.StepSuite.SetupTest()
+
+	s.step = &createRdmaRxeLinkStep{}
+	s.Cfg.Nodes = []config.Node{
+		{
+			Name: "node1",
+			Host: "1.1.1.1",
+		},
+	}
+	s.SetupRuntime()
+	s.step.Init(s.Runtime, s.MockEm, s.Cfg.Nodes[0])
+}
+
+func (s *createRdmaRxeLinkStepSuite) TestCreateRdmaRxeLinkStep() {
+	tmpDir := "/tmp/m3fs-prepare-network.123"
+	s.MockLocalFS.On("MkdirTemp", "/tmp", "m3fs-prepare-network.*").Return(tmpDir, nil)
+	scriptPath := tmpDir + "/create_rdma_rxe_link"
+	s.MockLocalFS.On("WriteFile", scriptPath,
+		[]byte(createRdmaLinkScript), os.FileMode(0755)).Return(nil)
+
+	binDir := s.Cfg.WorkDir + "/bin"
+	remoteScriptPath := binDir + "/create_rdma_rxe_link"
+	s.MockRunner.On("Exec", "mkdir", []string{"-p", binDir}).Return("", nil)
+	s.MockRunner.On("Scp", scriptPath, remoteScriptPath).Return(nil)
+	s.MockRunner.On("Exec", "chmod", []string{"+x", remoteScriptPath}).Return("", nil)
+	s.MockRunner.On("Exec", "bash", []string{remoteScriptPath}).Return("", nil)
+
+	s.MockLocalFS.On("RemoveAll", tmpDir).Return(nil)
+
+	s.NoError(s.step.Execute(s.Ctx()))
+
+	s.MockLocalFS.AssertExpectations(s.T())
+	s.MockRunner.AssertExpectations(s.T())
+}
